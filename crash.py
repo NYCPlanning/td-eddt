f06c9fe5833a55b765a45c22058b6d8941835f91
Yijun Ma <mayijun1203@gmail.com>
2022-07-23 16:16:33 -0400

    Update crash.py

--- crash.py
+++ crash.py
@@ -32,46 +32,46 @@ lionsp=lionsp.drop_duplicates(['segmentid'],keep='first').reset_index(drop=True)
 lionsp.to_file(path+'lionsp.geojson',driver='GeoJSON')
 
 
-# Crash Data
-df=pd.read_csv(path+'Motor_Vehicle_Collisions_-_Crashes.csv')
-df['date']=pd.to_datetime(df['CRASH DATE'],format='%m/%d/%Y',errors='coerce')
-df['year']=df['date'].dt.year
-df['month']=df['date'].dt.month
-df=df[(pd.notna(df['LATITUDE'])&(pd.notna(df['LONGITUDE']))&(df['LATITUDE']!=0)&(df['LONGITUDE']!=0))].reset_index(drop=True)
-df=df[(df['year']==2020)].reset_index(drop=True)
-df['lat']=df['LATITUDE'].copy()
-df['long']=df['LONGITUDE'].copy()
-df['pedinj']=df['NUMBER OF PEDESTRIANS INJURED'].copy()
-df['cycinj']=df['NUMBER OF CYCLIST INJURED'].copy()
-df['motinj']=df['NUMBER OF MOTORIST INJURED'].copy()
-df['totinj']=df['NUMBER OF PERSONS INJURED'].copy()
-df['pedkill']=df['NUMBER OF PEDESTRIANS KILLED'].copy()
-df['cyckill']=df['NUMBER OF CYCLIST KILLED'].copy()
-df['motkill']=df['NUMBER OF MOTORIST KILLED'].copy()
-df['totkill']=df['NUMBER OF PERSONS KILLED'].copy()
-df=df[['pedinj','cycinj','motinj','totinj','pedkill','cyckill','motkill','totkill','lat','long']].reset_index(drop=True)
-df=gpd.GeoDataFrame(df,geometry=[shapely.geometry.Point(x,y) for x,y in zip(df['long'],df['lat'])],crs=4326)
-puma=gpd.read_file(path+'puma.geojson')
-puma.crs=4326
-puma['puma']=puma[['GEOID10']].copy()
-puma=puma[['puma','geometry']].reset_index(drop=True)
-df=gpd.sjoin(df,puma,how='left',op='intersects')
-df=df.groupby(['puma'],as_index=False).agg({'pedinj':'sum','cycinj':'sum','motinj':'sum','totinj':'sum',
-                                            'pedkill':'sum','cyckill':'sum','motkill':'sum','totkill':'sum'}).reset_index(drop=True)
-nycbkpt20=gpd.read_file(path+'nycbkpt20.geojson',driver='GeoJSON')
-nycbkpt20.crs=4326
-pop=pd.read_csv(path+'pop20.csv',dtype={'blockid':str,'pop20':float})
-pop=pd.merge(nycbkpt20,pop,how='left',on='blockid')
-pop=pop.groupby(['puma'],as_index=False).agg({'pop20':'sum'}).reset_index(drop=True)
-df=pd.merge(df,pop,how='left',on='puma')
-lionsp=gpd.read_file(path+'lionsp.geojson')
-lionsp.crs=4326
-lionsp=gpd.overlay(lionsp,puma,how='intersection')
-lionsp=lionsp.to_crs(6539)
-lionsp['mile']=lionsp['geometry'].length/5280
-lionsp=lionsp.groupby(['puma'],as_index=False).agg({'mile':'sum'})
-df=pd.merge(df,lionsp,how='left',on='puma')
-df.to_csv(path+'crash.csv',index=False)
+# # Crash Data
+# df=pd.read_csv(path+'Motor_Vehicle_Collisions_-_Crashes.csv')
+# df['date']=pd.to_datetime(df['CRASH DATE'],format='%m/%d/%Y',errors='coerce')
+# df['year']=df['date'].dt.year
+# df['month']=df['date'].dt.month
+# df=df[(pd.notna(df['LATITUDE'])&(pd.notna(df['LONGITUDE']))&(df['LATITUDE']!=0)&(df['LONGITUDE']!=0))].reset_index(drop=True)
+# df=df[(df['year']==2020)].reset_index(drop=True)
+# df['lat']=df['LATITUDE'].copy()
+# df['long']=df['LONGITUDE'].copy()
+# df['pedinj']=df['NUMBER OF PEDESTRIANS INJURED'].copy()
+# df['cycinj']=df['NUMBER OF CYCLIST INJURED'].copy()
+# df['motinj']=df['NUMBER OF MOTORIST INJURED'].copy()
+# df['totinj']=df['NUMBER OF PERSONS INJURED'].copy()
+# df['pedkill']=df['NUMBER OF PEDESTRIANS KILLED'].copy()
+# df['cyckill']=df['NUMBER OF CYCLIST KILLED'].copy()
+# df['motkill']=df['NUMBER OF MOTORIST KILLED'].copy()
+# df['totkill']=df['NUMBER OF PERSONS KILLED'].copy()
+# df=df[['pedinj','cycinj','motinj','totinj','pedkill','cyckill','motkill','totkill','lat','long']].reset_index(drop=True)
+# df=gpd.GeoDataFrame(df,geometry=[shapely.geometry.Point(x,y) for x,y in zip(df['long'],df['lat'])],crs=4326)
+# puma=gpd.read_file(path+'puma.geojson')
+# puma.crs=4326
+# puma['puma']=puma[['GEOID10']].copy()
+# puma=puma[['puma','geometry']].reset_index(drop=True)
+# df=gpd.sjoin(df,puma,how='left',op='intersects')
+# df=df.groupby(['puma'],as_index=False).agg({'pedinj':'sum','cycinj':'sum','motinj':'sum','totinj':'sum',
+#                                             'pedkill':'sum','cyckill':'sum','motkill':'sum','totkill':'sum'}).reset_index(drop=True)
+# nycbkpt20=gpd.read_file(path+'nycbkpt20.geojson',driver='GeoJSON')
+# nycbkpt20.crs=4326
+# pop=pd.read_csv(path+'pop20.csv',dtype={'blockid':str,'pop20':float})
+# pop=pd.merge(nycbkpt20,pop,how='left',on='blockid')
+# pop=pop.groupby(['puma'],as_index=False).agg({'pop20':'sum'}).reset_index(drop=True)
+# df=pd.merge(df,pop,how='left',on='puma')
+# lionsp=gpd.read_file(path+'lionsp.geojson')
+# lionsp.crs=4326
+# lionsp=gpd.overlay(lionsp,puma,how='intersection')
+# lionsp=lionsp.to_crs(6539)
+# lionsp['mile']=lionsp['geometry'].length/5280
+# lionsp=lionsp.groupby(['puma'],as_index=False).agg({'mile':'sum'})
+# df=pd.merge(df,lionsp,how='left',on='puma')
+# df.to_csv(path+'crash.csv',index=False)
 
 
 
